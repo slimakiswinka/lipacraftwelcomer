9fe357d99cd21bddb982ab8457de48af2327ecfe
slimakiswinka <81806308+slimakiswinka@users.noreply.github.com>
2023-02-14 20:14:45 +0100

    Update LcWelcomer.java

--- src/main/java/net/fabricmc/lcw/LcWelcomer.java
+++ src/main/java/net/fabricmc/lcw/LcWelcomer.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class LcWelcomer implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -13,7 +13,9 @@ public class ExampleMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
     ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
-        client.inGameHud.getChatHud().addMessage(new LiteralText("Hello!"));
+        ITextComponent msg = new StringTextComponent("Hello!");
+UUID testuuid = Minecraft.getInstance().player.getUniqueID();
+Minecraft.getInstance().player.sendMessage(msg,testuuid);
     });
 }
 	
